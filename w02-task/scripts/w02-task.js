c3b49a1650a0905de64d8625836f932789e4381e
romerodav <romerodav@hotmail.com>
2023-10-28 20:43:11 -0300

    w02-task finished

---
+++ w02-task/scripts/w02-task.js
@@ -0,0 +1,35 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullName = "Jaime David Romero";
+let currentYear = "2023";
+/*let profilePicture = "images/yourprofileimagename.png";*/
+/*let profilePicture = "images/placeholder.png";*/
+let profilePicture = "images/Me20231026alas022318.jpg";
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById("name");
+const foodElement = document.getElementById("food");
+const yearElement = document.querySelector("#year");
+const imageElement = document.images[0];
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = currentYear;
+imageElement.setAttribute(`src`, profilePicture);
+imageElement.setAttribute(`alt`, `Profile image of ${profilePicture}`);
+
+/* Step 5 - Array */
+let favFoods = ["Milanesa","Empanadas","Tacos"];
+foodElement.setAttribute("food", favFoods);
+let anotherSingleFavFood = "Locro";
+foodElement.innerHTML += `<br>${favFoods}`;
+favFoods.push(anotherSingleFavFood);
+foodElement.innerHTML += `<br>${favFoods}`;
+favFoods.shift();
+foodElement.innerHTML += `<br>${favFoods}`;
+favFoods.pop();
+foodElement.innerHTML += `<br>${favFoods}`;
+
