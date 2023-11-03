17d3f3b3ffa99b5a6eb14d3bfdfd1587f767ec66
romerodav <romerodav@hotmail.com>
2023-11-03 03:56:04 -0300

    links corrected

--- w02-task/scripts/w02-task.js
+++ w02-task/scripts/w02-task.js
@@ -33,3 +33,7 @@ foodElement.innerHTML += `<br>${favFoods}`;
 favFoods.pop();
 foodElement.innerHTML += `<br>${favFoods}`;
 
+//links
+//document.querySelector(`<a href="w01-task.html">W1 Task</a>`).setAttribute(`href`, `../w01-task/w01-task-html`);
+document.querySelector(`#menu > li:nth-child(3) > a`).setAttribute(`href`, `../w01-task/w01-task.html`);
+document.querySelector(`#menu > li:nth-child(4) > a`).setAttribute(`href`, `../w03-task/w03-task.html`);
