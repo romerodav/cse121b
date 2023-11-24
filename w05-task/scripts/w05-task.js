3c50fa4f9c364915607e493ad2578c0aa9021763
romerodav <romerodav@hotmail.com>
2023-11-23 21:41:01 -0300

    w05-task finished

--- w05-task/scripts/w05-task.js
+++ w05-task/scripts/w05-task.js
@@ -2,16 +2,17 @@
 
 /* Declare and initialize global variables */
 const templesElement = document.getElementById(`temples`);
-let templeList = [];
+let templeList = [], templeCopy = [];
 
 /* async displayTemples Function */
 const displayTemples = (temples) =>
 {
-    templeList.forEach(temple =>
+    temples.forEach((temple) =>
         {
             let article = document.createElement('article');
             let h3 = document.createElement('h3');
-            h3 = temple.templeName;
+            let textNode = document.createTextNode(temple.templeName);            ;
+            h3.appendChild(textNode);
             let img = document.createElement(`img`);
             img.src = temple.imageUrl;
             img.alt = temple.location;
@@ -24,20 +25,55 @@ const displayTemples = (temples) =>
 /* async getTemples Function using fetch()*/
 const getTemples = async () =>
 {
-    const response = await fetch("https://byui-cse.github.io/cse121b-ww-course/resources/temples.json");
+    const response = await fetch(`https://byui-cse.github.io/cse121b-ww-course/resources/temples.json`);
+    if (response.ok)
+    {
+        templeList = await response.json();
+        templeCopy = [].concat(templeList);
+        displayTemples(templeList);
+    }
 }
 
 /* reset Function */
 
 
+
+function reset(temples)
+{
+    while(temples.length > 0)
+    {
+        temples.pop();
+        document.getElementById(`temples`).innerHTML=``;
+    }
+}
+
 /* sortBy Function */
 
-
-
-getTemples();
+const sortBy = (temples) =>
+{
+    reset(temples);
+    const sortByValue = document.getElementById(`sortBy`);
+    const filter = sortByValue.value;
+    temples = [].concat(templeCopy);
+    switch (filter)
+    {
+        case `utah`:
+            temples = temples.filter((temple) => temple.location.includes(`Utah`));
+          break;
+        case `notutah`:
+            temples = temples.filter((temple) => !temple.location.includes(`Utah`));
+          break;
+        case `older`:
+            temples = temples.filter((temple) => Date.parse(temple.dedicated) < Date.parse(1950, 0, 1));
+    }
+    displayTemples(temples);
+    templeList = [].concat(temples);
+}
 
 /* Event Listener */
+document.querySelector("#sortBy").addEventListener("change", () => {sortBy(templeList)});
 
+getTemples();
 
 //links corrections
 document.querySelector(`#menu > li:nth-child(2) > a`).setAttribute(`href`, `../test.html`);
